f66b10683b6932d80c3549e3d5c73eefab48a9ae
SantiJaime <santij03@gmail.com>
2026-09-16 18:44:03 -0300

    añado recursos de usuarios + configuracion de env

---
+++ backend/src/enums/usuario.enums.ts
@@ -0,0 +1,10 @@
+export enum EstadoUsuario {
+  ACTIVO = 'ACTIVO',
+  BAJA = 'BAJA',
+}
+
+export enum RolUsuario {
+  MEDICO = 'MEDICO',
+  PACIENTE = 'PACIENTE',
+  ADMINISTRADOR = 'ADMINISTRADOR',
+}
